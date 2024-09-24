72cc9c86897bfac7c09cfa171f3b5c9c4450785f
Usman salim <usman.salim@likeminds.community>
2024-09-24 17:17:46 +0530

    upgraded versions

--- likeminds-chat-reactnative-integration/ChatSX/setup.ts
+++ likeminds-chat-reactnative-integration/ChatSX/setup.ts
@@ -5,7 +5,7 @@ import AudioPlayer from "./optionalDependecies/AudioPlayer";
 
 export const initMyClient = (filterStateMessage: ConversationState[]) => {
   const myClient = LMChatClient.setfilterStateConversation(filterStateMessage)
-    .setVersionCode(35)
+    .setVersionCode(36)
     .build();
 
   Client.setMyClient(myClient);
