737e349f36debbb54872a993abdf5a291f8a6f3e
Usman salim <usman.salim@likeminds.community>
2024-09-27 15:17:45 +0530

    added listeners for notification

--- example/App.tsx
+++ example/App.tsx
@@ -4,6 +4,9 @@
  *
  * @format
  */
+// if (__DEV__) {
+//   require("./ReactotronConfig");
+// }
 
 import React, {useEffect, useState} from 'react';
 import {
@@ -16,6 +19,8 @@ import {
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {navigationRef} from './RootNavigation';
+import messaging, { firebase } from "@react-native-firebase/messaging";
+import notifee, { EventType } from "@notifee/react-native";
 import {
   CarouselScreen,
   CreatePollScreen,
@@ -39,6 +44,9 @@ import {
   DmAllMembers,
   initMyClient,
   SearchInChatroom,
+  getNotification,
+  Token,
+  getRoute,
 } from '@likeminds.community/chat-rn-core';
 import ChatroomScreenWrapper from './screens/Chatroom/ChatroomScreenWrapper';
 import {setStyles} from './styles';
@@ -64,6 +72,7 @@ import SearchInChatroomScreen from './screens/SearchInChatroom';
 import {ScreenName} from './src/enums/screenNameEnums';
 import {LMCoreCallbacks} from '@likeminds.community/chat-rn-core/ChatSX/setupChat';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import { pushAPI, token } from './pushNotification';
 
 const Stack = createNativeStackNavigator();
 
@@ -89,6 +98,7 @@ class CustomCallbacks implements LMChatCallbacks, LMChatroomCallbacks {
 const lmChatInterface = new CustomCallbacks();
 
 function App(): React.JSX.Element {
+  const [FCMToken,setFCMToken] = useState("");
   const chatroomId = '';
   const profileImageUrl = '';
   const [users, setUsers] = useState<any>();
@@ -182,6 +192,85 @@ function App(): React.JSX.Element {
       };
     },
   );
+
+   /// Setup notifications
+   useEffect(() => {
+    token().then((res) => {
+      if (!!res) {
+        setFCMToken(res);
+      }
+    });
+  }, []);
+  useEffect(() => {
+    async function get(){
+      const payload: InitUserWithUuid = {
+        userName: userName,
+        uuid: userUniqueID,
+        apiKey: apiKey,
+        isGuest: false,
+      };
+      const initiateUserResponse = await myClient?.initiateUser(payload);
+      const accessToken = initiateUserResponse?.accessToken;
+
+      if (FCMToken) {
+        pushAPI(FCMToken, accessToken);
+      }
+    }
+    get();
+  }, [FCMToken]);
+
+  // useEffect(() => {
+  //   console.log(firebase.messaging().isDeviceRegisteredForRemoteMessages,"regis")
+  //   firebase.app().messaging().onMessage(async (remoteMessage) => {
+  //     console.log("remote",remoteMessage)
+  //   })
+  //   firebase.messaging().onMessage(async (remote) => {
+  //     console.log("got it")
+  //   })
+  // },[])
+
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+      console.log("remotee message",remoteMessage)
+      const val = await getNotification(remoteMessage);
+      return val;
+    });
+
+    notifee.onForegroundEvent(async ({ type, detail }) => {
+      if (detail?.notification?.data?.route != undefined) {
+        const navigation = navigationRef?.current;
+        let currentRoute = navigation?.getCurrentRoute();
+        let routes = await getRoute(detail?.notification?.data?.route);
+
+        if (type === EventType.PRESS) {
+          if (!!navigation) {
+            if ((currentRoute?.name as any) === routes?.route) {
+              if (
+                JSON.stringify(routes?.params) !==
+                JSON.stringify(currentRoute?.params)
+              ) {
+                const popAction = StackActions.pop(1);
+                navigation.dispatch(popAction);
+                setTimeout(() => {
+                  navigation.navigate(
+                    routes?.route as never,
+                    routes?.params as never
+                  );
+                }, 1000);
+              }
+            } else {
+              navigation.navigate(
+                routes?.route as never,
+                routes?.params as never
+              ); //navigate(CHATROOM, {chatroomID: 69285});
+            }
+          }
+        }
+      }
+    });
+
+    return unsubscribe;
+  }, []);
 
   return (
     <GestureHandlerRootView style={{flex: 1}}>
--- example/index.js
+++ example/index.js
@@ -6,6 +6,20 @@ import App from './App';
 import {name as appName} from './app.json';
 import {RealmProvider} from '@realm/react';
 import {LoginSchemaRO} from './login/loginSchemaRO';
+import messaging, { firebase } from "@react-native-firebase/messaging";
+
+messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+  console.log(remoteMessage,"heree is messageee")
+  let val = await getNotification(remoteMessage);
+  return val;
+ });
+ messaging().onMessage(async (remoteMessage) => {
+  console.log(remoteMessage)
+  let val = await getNotification(remoteMessage);
+  return val
+ })
+//  firebase.messaging().onMessage(()=>{console.log("eerer")});
+//  firebase.app().messaging().onMessage(()=>console.log("got itttt"))
 
 const WrappedApp = () => (
   <RealmProvider schema={[LoginSchemaRO]}>
