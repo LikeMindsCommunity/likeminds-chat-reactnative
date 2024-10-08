d15f52b9ccd3e02914d9666c5d825756f751f308
Usman salim <usman.salim@likeminds.community>
2024-10-08 23:55:40 +0530

    added logic to open chatroom from notification

--- example/index.js
+++ example/index.js
@@ -7,11 +7,47 @@ import {name as appName} from './app.json';
 import {RealmProvider} from '@realm/react';
 import {LoginSchemaRO} from './login/loginSchemaRO';
 import messaging, { firebase } from "@react-native-firebase/messaging";
-import { getNotification } from '@likeminds.community/chat-rn-core';
+import { getNotification, getRoute } from '@likeminds.community/chat-rn-core';
+import { navigationRef } from './RootNavigation';
+import notifee, { EventType } from "@notifee/react-native";
+import { StackActions } from '@react-navigation/native';
 
 messaging().setBackgroundMessageHandler(async (remoteMessage) => {
   getNotification(remoteMessage)
  });
+
+notifee.onBackgroundEvent(async ({ type, detail }) => {
+  if (detail?.notification?.data?.route != undefined) {
+    const navigation = navigationRef?.current;
+    let currentRoute = navigation?.getCurrentRoute();
+    let routes = await getRoute(detail?.notification?.data?.route);
+
+    if (type === EventType.PRESS) {
+      if (!!navigation) {
+        if ((currentRoute?.name) === routes?.route) {
+          if (
+            JSON.stringify(routes?.params) !==
+            JSON.stringify(currentRoute?.params)
+          ) {
+            const popAction = StackActions.pop(1);
+            navigation.dispatch(popAction);
+            setTimeout(() => {
+              navigation.navigate(
+                routes?.route,
+                routes?.params
+              );
+            }, 1000);
+          }
+        } else {
+          navigation.navigate(
+            routes?.route,
+            routes?.params
+          ); //navigate(CHATROOM, {chatroomID: 69285});
+        }
+      }
+    }
+  }
+})
 
 const WrappedApp = () => (
   <RealmProvider schema={[LoginSchemaRO]}>
