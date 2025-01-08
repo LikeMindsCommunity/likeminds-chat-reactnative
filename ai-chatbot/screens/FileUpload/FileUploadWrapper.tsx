dc8abef7350bae3523b5d1b5cae37bb427903c7e
Usman salim <usman.salim@likeminds.community>
2025-01-09 01:19:43 +0530

    updated fileUploadWrapper and added missing component

--- ai-chatbot/screens/FileUpload/FileUploadWrapper.tsx
+++ ai-chatbot/screens/FileUpload/FileUploadWrapper.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import FileUploadScreen from '.';
 import {ChatroomContextProvider} from '@likeminds.community/chat-rn-core/ChatSX/context';
+import { FileUploadContextProvider } from '@likeminds.community/chat-rn-core/ChatSX/context/FileUploadContext';
 
 function FileUploadScreenWrapper() {
   return (
     <ChatroomContextProvider>
-      <FileUploadScreen />
+      <FileUploadContextProvider>
+        <FileUploadScreen />
+      </FileUploadContextProvider>
     </ChatroomContextProvider>
   );
 }
